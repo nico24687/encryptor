e26a913d974610feed8b9a82a9a648a69e9d8a99
nico24687 <theonlynico@hotmail.com>
2017-09-03 15:48:05 +0000

    removing the comments

--- encryptor.rb
+++ encryptor.rb
@@ -18,17 +18,12 @@ class Encryptor
     end
   
     def encrypt(string)
-      # 1. Cut the input string into letters
       letters = string.split("")
-    
-      # 2. Encrypt those letters one at a time, gathering the results
       results = []
       letters.each do |letter|
         encrypted_letter = encrypt_letter(letter)
         results.push(encrypted_letter)
       end
-    
-      # 3. Join the results back together in one string
       results.join
     end
     
