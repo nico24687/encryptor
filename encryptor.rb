50a6586600bb8c2199ad35dc906361f4bd04ac40
nico24687 <theonlynico@hotmail.com>
2017-09-04 19:19:54 +0000

    changing the encrpyt method to allow for rotation

--- encryptor.rb
+++ encryptor.rb
@@ -1,20 +1,16 @@
 #http://tutorials.jumpstartlab.com/projects/encryptor.html
-
 class Encryptor
     
-     def cipher
-    {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
-     'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
-     'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
-     'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
-     'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
-     'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
-     'y' => 'l', 'z' => 'm'}
-     end
-  
+     def cipher(rotation)
+      characters = (' '..'z').to_a
+      rotated_characters = characters.rotate(rotation)
+      Hash[characters.zip(rotated_characters)]
+    end
+    
     def encrypt_letter(letter)
-      lowercase_letter = letter.downcase
-      cipher[lowercase_letter]
+      rotation = 13
+      cipher_for_rotation = cipher(rotation)
+      cipher_for_rotation[letter]
     end
     
     def decrypt_letter(letter)
@@ -39,3 +35,8 @@ end
 
 
 
+
+# Define the amount to rotate
+# Create an array of our list of characters.
+# Create a second array that is a list of characters rotated by the amount to rotate.
+# Create a Hash with the first list as the keys and the second list as the values.
