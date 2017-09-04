9f430f4065cc51b0b07e61c4543b699b7e0894d8
nico24687 <theonlynico@hotmail.com>
2017-09-04 19:44:33 +0000

    now able to use the encrpt letter method with any rotation value

--- encryptor.rb
+++ encryptor.rb
@@ -9,8 +9,7 @@ class Encryptor
       Hash[characters.zip(rotated_characters)]
     end
     
-    def encrypt_letter(letter)
-      rotation = 13
+    def encrypt_letter(letter,rotation)
       cipher_for_rotation = cipher(rotation)
       cipher_for_rotation[letter]
     end
